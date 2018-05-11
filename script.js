7e7e308272fcd0c5c696c87f9041ec36fb762ecc
Jacob Janak <jakeathotmail@gmail.com>
2018-05-11 14:22:51 -0700

    Almost done adding a demo. Just testing it on GitHub pages.

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 $(function() {
 
-  let i; // for looping
-  let imgCount = 1; // keeps track of imgs on page
-  const imgWidth = $('#landscape').width(); // width of each img
+  // GLOBAL VARIABLES
+  let i, j; // for looping
 
-  // returns true if number is even or false if odd
+  // UTILITIES
   const isEven = num => num % 2 === 0;
 
-  // adds imgs and flipped imgs to DOM as necessary
-  const remakeHeader = () => {
+  // INFINITE IMAGE
+  const infiniteImage = () => {
+    const browserWidth = $(window).width();
 
-    // get current width of the browser
-    const browserWidth = $(document).width();
+    //
+    $.each($('.infinite-image'), function() {
+      const imgsNeeded = Math.ceil(browserWidth / $(this).width());
+      const imgsExisting = $(this).siblings('.infinite-copy').length + 1;
 
-    // if browser is bigger than one img
-    if (browserWidth > imgWidth) {
+      //
+      for (i = imgsExisting + 1; i <= imgsNeeded; i++) {
+        const $newImg = $('<img>');
+        $newImg.addClass('infinite-copy')
+        $newImg.attr('src', $(this).attr('src'))
 
-      // how many times will we need to add a new image
-      const times = Math.floor(browserWidth / imgWidth) - imgCount;
+        // flip every other image
+        if (isEven(i)) $newImg.addClass('flipped');
 
-      for (i = 0; i < times; i++) {
-
-        // create a new image which may be flipped
-        let $img = $('<img>');
-        $img.attr('src', 'http://www.hdwallpaperup.com/wp-content/uploads/2015/01/Snowy-Forest-Reflection.jpg')
-        if (isEven(i)) $img.addClass('flipped');
-
-        // add to DOM and update number of imgs
-        $('header').append($img)
-        imgCount++
+        // add to DOM
+        $(this).parent().append($newImg)
       }
-    }
+    })
   }
 
-  // call function on initial page load
-  remakeHeader()
-
-  // also call function whenever the browser gets resized
-  $(window).on('resize', remakeHeader)
-
+  // USAGE
+  infiniteImage()
+  $(window).on('resize', infiniteImage)
 })
