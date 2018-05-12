764becf03887dc1489843ff6e9cbb2e000aa59a0
Jacob Janak <jakeathotmail@gmail.com>
2018-05-12 15:32:12 -0700

    Fixed bug where my code doens't wait for images to load.

--- script.js
+++ script.js
@@ -9,28 +9,45 @@ $(document).ready(function() {
   // INFINITE IMAGE
   const infiniteImage = () => {
     const browserWidth = $(window).width();
+    let isDone = true;
+
+    // j will be used to keep track of index for .complete
+    j = 0;
 
     // for each infinite image find how many more copies are needed
     $.each($('.infinite-image'), function() {
-      const imgsNeeded = Math.ceil(browserWidth / $(this).width());
-      const imgsExisting = $(this).siblings('.infinite-copy').length + 1;
+      if (document.getElementsByClassName('infinite-image')[j].complete) {
+        const imgsNeeded = Math.ceil(browserWidth / $(this).width());
+        const imgsExisting = $(this).siblings('.infinite-copy').length + 1;
 
-      // create a copy
-      for (i = imgsExisting + 1; i <= imgsNeeded; i++) {
-        const $newImg = $('<img>');
-        $newImg.addClass('infinite-copy')
-        $newImg.attr('src', $(this).attr('src'))
+        // create a copy
+        for (i = imgsExisting + 1; i <= imgsNeeded; i++) {
+          const $newImg = $('<img>');
+          $newImg.addClass('infinite-copy')
+          $newImg.attr('src', $(this).attr('src'))
 
-        // flip every other image
-        if (isEven(i)) $newImg.addClass('flipped');
+          // flip every other image
+          if (isEven(i)) $newImg.addClass('flipped');
 
-        // add to DOM
-        $(this).parent().append($newImg)
+          // add to DOM
+          $(this).parent().append($newImg)
+        }
+      } else {
+        isDone = false;
       }
+      j++
     })
+    return isDone;
   }
 
   // USAGE
-  infiniteImage()
+  if (!infiniteImage()) {
+    // if it didn't work the first time then wait for images to load
+    const interval = setInterval(function() {
+      const isDone = infiniteImage();
+      if (infiniteImage()) { clearInterval(interval) };
+    }, 50);
+  }
+
   $(window).on('resize', infiniteImage)
 })
